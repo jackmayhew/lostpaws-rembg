13c8f317104de3ed777fe03a0cefe74becd2481b
jackmayhew <jacksonmayhew9@gmail.com>
2024-12-20 18:24:22 -0500

    api

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile
+from fastapi.responses import Response
 
 app = FastAPI()
 
 @app.get("/")
 async def root():
     return {"message": "I'm alive"}
+
+@app.post("/remove-bg")
+async def remove_background(file: UploadFile):
+    input_image = await file.read()
+    # Just return the image without processing for now
+    return Response(input_image, media_type="image/png")
