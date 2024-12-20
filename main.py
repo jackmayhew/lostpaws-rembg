094bc7537d91273a71aaacb8e0fb83b80062d66c
jackmayhew <jacksonmayhew9@gmail.com>
2024-12-20 18:10:22 -0500

    gunicorn

--- main.py
+++ main.py
@@ -1,23 +1,19 @@
 from fastapi import FastAPI, File, UploadFile
-from fastapi.middleware.cors import CORSMiddleware
 from rembg import remove
 from fastapi.responses import Response
 
 app = FastAPI()
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["http://localhost:3000", "https://lostpaws.netlify.app/"], 
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+@app.get("/")  # Add a root endpoint
+async def root():
+    return {"status": "ok"}
+
+@app.get("/health")
+def health_check():
+    return {"status": "ok"}
 
 @app.post("/remove-bg")
 async def remove_background(file: UploadFile):
     input_image = await file.read()
     output_image = remove(input_image)
     return Response(output_image, media_type="image/png")
-
-@app.get("/health")
-def health_check():
-    return {"status": "ok"}
