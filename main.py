2e6e4df75ad2bbd2a7a1c7437cf7c2c24d650ebf
jackmayhew <jacksonmayhew9@gmail.com>
2024-12-20 18:43:35 -0500

    changes

--- main.py
+++ main.py
@@ -1,29 +1,23 @@
-import os
-os.environ['U2NET_HOME'] = '/opt/models/'
-
 from fastapi import FastAPI, File, UploadFile
-from fastapi.responses import Response
+from fastapi.middleware.cors import CORSMiddleware
 from rembg import remove
-import logging
-
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
+from fastapi.responses import Response
 
 app = FastAPI()
 
-@app.get("/")
-async def root():
-    return {"message": "I'm alive"}
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000", "https://lostpaws.netlify.app/", "*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
-@app.post("/remove-bg")
-async def remove_background(file: UploadFile):
-    logger.info("Got image upload request")
-    try:
-        input_image = await file.read()
-        logger.info("Read image file")
-        output_image = remove(input_image)
-        logger.info("Successfully removed background")
-        return Response(output_image, media_type="image/png")
-    except Exception as e:
-        logger.error(f"Error in remove_background: {str(e)}")
-        raise
+# @app.post("/remove-bg")
+# async def remove_background(file: UploadFile):
+#     input_image = await file.read()
+#     output_image = remove(input_image)
+#     return Response(output_image, media_type="image/png")
+
+@app.get("/health")
+def health_check():
+    return {"status": "ok"}
