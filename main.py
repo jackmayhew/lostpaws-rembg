82a81774253f00524a5fe18ee5fdfddbb7fd2294
jackmayhew <jacksonmayhew9@gmail.com>
2024-12-20 18:50:13 -0500

    api

--- main.py
+++ main.py
@@ -5,18 +5,22 @@ from fastapi.responses import Response
 
 app = FastAPI()
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["http://localhost:3000", "https://lostpaws.netlify.app/", "*"],
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=["http://localhost:3000", "https://lostpaws.netlify.app/", "*"],
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
 
 # @app.post("/remove-bg")
 # async def remove_background(file: UploadFile):
 #     input_image = await file.read()
 #     output_image = remove(input_image)
 #     return Response(output_image, media_type="image/png")
+
+@app.get("/")
+async def root():
+    return {"message": "I'm alive"}
 
 @app.get("/health")
 def health_check():
