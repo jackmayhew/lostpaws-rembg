bd7edb394a54b8f2924c87fb6c48462a1f197897
jackmayhew <jacksonmayhew9@gmail.com>
2024-12-20 18:19:43 -0500

    api

--- main.py
+++ main.py
@@ -1,19 +1,7 @@
-from fastapi import FastAPI, File, UploadFile
-from rembg import remove
-from fastapi.responses import Response
+from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/")  # Add a root endpoint
+@app.get("/")
 async def root():
-    return {"status": "ok"}
-
-@app.get("/health")
-def health_check():
-    return {"status": "ok"}
-
-@app.post("/remove-bg")
-async def remove_background(file: UploadFile):
-    input_image = await file.read()
-    output_image = remove(input_image)
-    return Response(output_image, media_type="image/png")
+    return {"message": "I'm alive"}
